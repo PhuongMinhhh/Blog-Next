9b4b5b16da8b18f1dbedea7d8c14dd198e6ad1b9
Minh Phương <51900182@student.tdtu.edu.vn>
2023-06-13 22:53:50 +0700

    Home update

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,7 +1,22 @@
 import React from "react";
-
+import styles from "./page.module.css";
+import Image from "next/image";
+import github from "../../../public/github.png";
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.imgContainer}>
+        <Image
+          
+          height={500}
+          src={github}
+          alt=""
+          className={styles.img}
+        />
+      </div>
+      <div className={styles.texContainer}></div>
+    </div>
+  );
 };
 
 export default About;
