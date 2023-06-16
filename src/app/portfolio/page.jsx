2fdccf7f20c28e2baab4ff7d1a3f902f0e96d539
phuongminhhh <hastephuongminh1@gmail.com>
2023-06-16 07:43:18 +0700

    porfolio update

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -1,5 +1,17 @@
+import React from "react";
+import Link from "next/link";
+import styles from "./page.module.css";
 const Portfolio = () => {
-  return <div>page</div>;
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.selectTitle}>
+        <div className={styles.items}>
+          <Link className={styles.item} href="/portfolio/illustrations"></Link>
+          <span className={styles.title}>Illustrations</span>
+        </div>
+      </h1>
+    </div>
+  );
 };
 
 export default Portfolio;
