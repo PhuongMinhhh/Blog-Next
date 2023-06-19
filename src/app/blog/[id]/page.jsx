7f98655305cd1c30bc46bc292dd0fd9431e3636c
Minh Phương <51900182@student.tdtu.edu.vn>
2023-06-19 14:52:51 +0700

    api blog

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -53,27 +53,53 @@ const BlogPost = async ({ params }) => {
         </div>
       </div>
       <div className={styles.content}>
-        <p className={styles.text}>The Bad Developer Joke
-A Facebook post said something like, “Giving users the ability to work with AIs to code will mean that users will finally need to articulate what they want, so your jobs are safe.”
-
-The implication was that users generally don’t know what they want, so giving them the ability to create directly with AI will end badly. However, both my experience and this joke highlight the underlying issue that programmers and users lack training in collaborating with each other.
-
-Part of the underlying problem is that programmers typically have little interest in business operations, and operations employees have little interest in coding. Since neither side generally wants to learn the nuances of the other, this can lead to some very unsatisfied users and very frustrated programmers.
-
-AI has the capability to break through this problem because, as it advances, it will naturally attempt to learn about the user and be better able, over time, to provide an outcome closer to what the user should want.
-
-I say “should” because, in my experience, often one of the problems when creating an app is that the user hasn’t thought through what they want entirely. Only after seeing the draft app will they suddenly realize that what they want isn’t anything like what they got.
-
-AI gets around this problem by not having a personality, so it doesn’t get irritated, angry, or frustrated. It learns through iteration and is willing to iterate infinitely to approach closing unmet user needs.
-
-But users and programmers will still need to develop competence with the tool. Otherwise, they will likely become frustrated with the endless iterations that result because the user cannot completely articulate what they want, and particularly what they don’t want, in the new app.
-
-Windows 11 Baseline
-By placing generative AI into Windows, Microsoft creates a forcing function trend where users will learn how to work with generative AI to get better results. They will have to learn to fully articulate their needs to lower the number of annoying iterations the AI has to go through to understand those needs, and, most importantly, users will have to develop the skills needed to understand and communicate what they want.
-
-We’ve had some mixed results with this kind of thing. Boolean logic is what the internet has used to refine searches. Those who learned Boolean logic found they could get the results they wanted far more quickly than those who didn’t. Still, we aren’t exactly up to our necks in Boolean logic users on the web, showcasing that the weak link remains users who refuse to learn the skills needed to become more efficient.
-
-The difference with AI, however, is that AI can help bridge the gap by learning what makes a particular user unique and attempting to bridge the knowledge and experience gap. Unlike Boolean logic, which is static, the AI will evolve to become a much more personalized interface for the user and substantially reduce the need for the user to have a unique AI communications skill set.</p>
+        <p className={styles.text}>
+          The Bad Developer Joke A Facebook post said something like, “Giving
+          users the ability to work with AIs to code will mean that users will
+          finally need to articulate what they want, so your jobs are safe.” The
+          implication was that users generally don’t know what they want, so
+          giving them the ability to create directly with AI will end badly.
+          However, both my experience and this joke highlight the underlying
+          issue that programmers and users lack training in collaborating with
+          each other. Part of the underlying problem is that programmers
+          typically have little interest in business operations, and operations
+          employees have little interest in coding. Since neither side generally
+          wants to learn the nuances of the other, this can lead to some very
+          unsatisfied users and very frustrated programmers. AI has the
+          capability to break through this problem because, as it advances, it
+          will naturally attempt to learn about the user and be better able,
+          over time, to provide an outcome closer to what the user should want.
+          I say “should” because, in my experience, often one of the problems
+          when creating an app is that the user hasn’t thought through what they
+          want entirely. Only after seeing the draft app will they suddenly
+          realize that what they want isn’t anything like what they got. AI gets
+          around this problem by not having a personality, so it doesn’t get
+          irritated, angry, or frustrated. It learns through iteration and is
+          willing to iterate infinitely to approach closing unmet user needs.
+          But users and programmers will still need to develop competence with
+          the tool. Otherwise, they will likely become frustrated with the
+          endless iterations that result because the user cannot completely
+          articulate what they want, and particularly what they don’t want, in
+          the new app. Windows 11 Baseline By placing generative AI into
+          Windows, Microsoft creates a forcing function trend where users will
+          learn how to work with generative AI to get better results. They will
+          have to learn to fully articulate their needs to lower the number of
+          annoying iterations the AI has to go through to understand those
+          needs, and, most importantly, users will have to develop the skills
+          needed to understand and communicate what they want. We’ve had some
+          mixed results with this kind of thing. Boolean logic is what the
+          internet has used to refine searches. Those who learned Boolean logic
+          found they could get the results they wanted far more quickly than
+          those who didn’t. Still, we aren’t exactly up to our necks in Boolean
+          logic users on the web, showcasing that the weak link remains users
+          who refuse to learn the skills needed to become more efficient. The
+          difference with AI, however, is that AI can help bridge the gap by
+          learning what makes a particular user unique and attempting to bridge
+          the knowledge and experience gap. Unlike Boolean logic, which is
+          static, the AI will evolve to become a much more personalized
+          interface for the user and substantially reduce the need for the user
+          to have a unique AI communications skill set.
+        </p>
       </div>
     </div>
   );
--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -2,54 +2,39 @@ import React from "react";
 import Image from "next/image";
 import styles from "./page.module.css";
 import Link from "next/link";
-const Post = () => {
+async function getData() {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts`, {
+    cache: "no-store",
+  });
+
+  if (!res.ok) {
+    return notFound();
+  }
+
+  return res.json();
+}
+
+const Post = async () => {
+  const data = await getData();
   return (
     <div className={styles.container}>
-      <Link href="/blog/testID" className={styles.container}>
-        <div className={styles.imgContainer}>
-          <Image
-            src="https://www.technewsworld.com/wp-content/uploads/sites/3/2022/09/Windows-11.jpg"
-            alt=""
-            width={400}
-            height={250}
-            className={styles.image}
-          ></Image>
-        </div>
-        <div className={styles.content}>
-          <h1 className={styles.title}>Test</h1>
-          <p className={styles.desc}>Desc</p>
-        </div>
-      </Link>
-      <Link href="/blog/testID" className={styles.container}>
-        <div className={styles.imgContainer}>
-          <Image
-            src="https://www.technewsworld.com/wp-content/uploads/sites/3/2022/09/Windows-11.jpg"
-            alt=""
-            width={400}
-            height={250}
-            className={styles.image}
-          ></Image>
-        </div>
-        <div className={styles.content}>
-          <h1 className={styles.title}>Test</h1>
-          <p className={styles.desc}>Desc</p>
-        </div>
-      </Link>
-      <Link href="/blog/testID" className={styles.container}>
-        <div className={styles.imgContainer}>
-          <Image
-            src="https://www.technewsworld.com/wp-content/uploads/sites/3/2022/09/Windows-11.jpg"
-            alt=""
-            width={400}
-            height={250}
-            className={styles.image}
-          ></Image>
-        </div>
-        <div className={styles.content}>
-          <h1 className={styles.title}>Test</h1>
-          <p className={styles.desc}>Desc</p>
-        </div>
-      </Link>
+      {data.map((item) => (
+        <Link href="/blog/testID" className={styles.container} key={item.id}>
+          <div className={styles.imgContainer}>
+            <Image
+              src="https://www.technewsworld.com/wp-content/uploads/sites/3/2022/09/Windows-11.jpg"
+              alt=""
+              width={400}
+              height={250}
+              className={styles.image}
+            />
+          </div>
+          <div className={styles.content}>
+            <h1 className={styles.title}>{item.title}</h1>
+            <p className={styles.desc}>{item.body}</p>
+          </div>
+        </Link>
+      ))}
     </div>
   );
 };
